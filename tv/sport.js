799be68a261b9d3056c83055a16de53cab6fc24c
oziltv <128941521+oziltv@users.noreply.github.com>
2023-05-13 00:04:45 +0700

    Update sport.js

--- tv/sport.js
+++ tv/sport.js
@@ -24,3 +24,6 @@ function bein3(){document.getElementById("tv")
 
 function laliga(){document.getElementById("tv")
 .innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTA0My9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function ssc1(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9zc2MtMS1vbi1wcmVtLWFrLmFrYW1haXplZC5uZXQvb3V0L3YxL2VjOTM4OTU3ZGEyODQ5ODc5ZjBjZmFjN2UzMDlmZjM4L2luZGV4Lm1wZA&key=ZTc2YWUxN2Q4NjE1NDZiZTljMjM4ZTY1ODI1MDljMmM&key2=ZGUxYTkxZTIzMDZiZjg0NDIyMzZhODA0NzdmNTI2ZTE" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     

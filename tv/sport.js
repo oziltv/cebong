08df02cac1a730d5be904e1edae24479f9cb781f
oziltv <128941521+oziltv@users.noreply.github.com>
2023-05-12 23:58:42 +0700

    Update sport.js

--- tv/sport.js
+++ tv/sport.js
@@ -1,2 +1,26 @@
-function daznnes1(){document.getElementById("tv")
+function daznes1(){document.getElementById("tv")
 .innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTAzNy9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function daznes2(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTAzOC9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function daznde1(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTAxNy9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function daznde2(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTAxOC9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function daznzona(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTAyNC9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function bein1(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTAzOS9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function bein2(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTA0MC9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function bein3(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTA0MS9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function laliga(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTA0My9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     

1cddb14aa8f09bffdd58f379736e1c27f887a665
oziltv <128941521+oziltv@users.noreply.github.com>
2023-05-12 16:42:29 +0700

    Create sport.js

---
+++ tv/sport.js
@@ -0,0 +1,2 @@
+function daznnes1(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9kY2UtZnMtbGl2ZS1kYXpuLWNkbi5kYXpuLmNvbS9kYXNoZHJtL2Rhem4tbGluZWFyLTAzNy9zdHJlYW0ubXBk&key=ODJmOGUyYTE3ZGFjNDRjMGExOGY2NjA0NzkzNDljNTk=&key2=MWIxNDRmNzNlNmZlZmU5MWNkMDVmODUwZTJiNTg5ZDA=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     

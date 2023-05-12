455540bf30e6f1121db69f55e2335fdb2cf255db
oziltv <128941521+oziltv@users.noreply.github.com>
2023-05-13 00:26:23 +0700

    Update sport.js

--- tv/sport.js
+++ tv/sport.js
@@ -27,3 +27,32 @@ function laliga(){document.getElementById("tv")
 
 function ssc1(){document.getElementById("tv")
 .innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9zc2MtMS1vbi1wcmVtLWFrLmFrYW1haXplZC5uZXQvb3V0L3YxL2VjOTM4OTU3ZGEyODQ5ODc5ZjBjZmFjN2UzMDlmZjM4L2luZGV4Lm1wZA&key=ZTc2YWUxN2Q4NjE1NDZiZTljMjM4ZTY1ODI1MDljMmM&key2=ZGUxYTkxZTIzMDZiZjg0NDIyMzZhODA0NzdmNTI2ZTE" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function vprem(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9jZC1zdHJlYW0tbGl2ZS50ZWxlbm9yY2RuLm5ldC9jZGdvL2NkZ29fdmlhc2F0X3Nwb3J0X3ByZW1pdW1fbGl2ZS9jZGdvX3ZpYXNhdF9zcG9ydF9wcmVtaXVtX2xpdmUuaXNtbC9wbGF5bGlzdC5tcGQ&key=YTE0NDdjZWNlZjNhNGU4MzlkNTljMWY5OTRlYWYyYzY&key2=ODZhMzc2Mjg4NDk2YTBjMzJlMTE3OGU0MjEzOWNlNjY" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function vsportfootbal(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get= " allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function vsportextra(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get= " allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function tv3max(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9jZC1zdHJlYW0tbGl2ZS50ZWxlbm9yY2RuLm5ldC9jZGdvL2NkX3RzYXRfdHYzX21heF9saXZlL2NkX3RzYXRfdHYzX21heF9saXZlLmlzbWwvcGxheWxpc3QubXBk&key=YTE0NDdjZWNlZjNhNGU4MzlkNTljMWY5OTRlYWYyYzY&key2=ODZhMzc2Mjg4NDk2YTBjMzJlMTE3OGU0MjEzOWNlNjY" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function sonympd1(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9jZG4tMDEudG9mZmVlbGl2ZS5jb20vb3JpZ2luLTA5L2xpdmUtb3JpZ2luL3NtaWw6c29ueV90ZW4xX2hkLnNtaWwvbWFuaWZlc3QubXBk&key=Y2UxNzI2NGIzMTdkYjEwOGYxOWNkYzExYWExYTllNjY&key2=YTIxMTg4YWVlOGZjNWM1NmQwMTZmY2ZmY2M2YjIyOTU" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function sonympd2(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9jZG4tMDEudG9mZmVlbGl2ZS5jb20vb3JpZ2luLTA5L2xpdmUtb3JpZ2luL3NtaWw6c29ueV90ZW4yX2hkLnNtaWwvbWFuaWZlc3QubXBk&key=Y2UxNzI2NGIzMTdkYjEwOGYxOWNkYzExYWExYTllNjY&key2=YTIxMTg4YWVlOGZjNWM1NmQwMTZmY2ZmY2M2YjIyOTU" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function sonympd3(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9jZG4tMDEudG9mZmVlbGl2ZS5jb20vb3JpZ2luLTA5L2xpdmUtb3JpZ2luL3NtaWw6c29ueV90ZW4zX2hkLnNtaWwvbWFuaWZlc3QubXBk&key=Y2UxNzI2NGIzMTdkYjEwOGYxOWNkYzExYWExYTllNjY&key2=YTIxMTg4YWVlOGZjNWM1NmQwMTZmY2ZmY2M2YjIyOTU" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+function espndeportes(){document.getElementById("tv")
+.innerHTML='<iframe src="https://corsproxy.io/?https://dsopgratis.eu.org/play/drm?get=aHR0cHM6Ly9jZG4ubGl2ZS52aWRnby5jb20vQ29udGVudC9EQVNIX1dWL0xpdmUvY2hhbm5lbCg3MTkxNC1FU1BOREhEKS9tYW5pZmVzdC5tcGQ&key=MmQ4NDZmMTNjNWJkNDNmM2E0NTZkOWU5NTg0NDdmMTc&key2=NDdlNTllMGNjYjI4YjBmZmE3Y2U4MzBmZDI1MTk1Mzk=" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}     
+
+
+
+
+
